873ec2a120809a8e1012ad8d07aea6e003862edb
Koray Ozkal <koryozkal@gmail.com>
2020-05-13 07:53:52 +0000

    first commit

---
+++ lib/mycomics_sale.rb
@@ -0,0 +1,6 @@
+require "mycomics_sale/version"
+
+module MycomicsSale
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mycomics_sale/version.rb
@@ -0,0 +1,3 @@
+module MycomicsSale
+  VERSION = "0.1.0"
+end
