53afe81e3bb24463fce5ea92ba12720fc0e5bf8b
Koray Ozkal <koryozkal@gmail.com>
2020-05-14 02:14:46 +0000

    CLI file update

--- lib/Sale.rb
+++ lib/Sale.rb
@@ -19,7 +19,8 @@ class Sale
 end
 
 def self.print_all_sales
-Sale.all.each do |sale|
+@@all.each_with_index do |sale, index|
+puts "=== SALE CAMPAIGN ##{index + 1} ==="
 puts "Book Title:"  
 puts sale.book_title
 puts sale.book_number
