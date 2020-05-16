50c1f0750178f424829f5508289711fb212eaf89
Koray Ozkal <koryozkal@gmail.com>
2020-05-16 02:01:47 +0000

    CLI file update

--- lib/MyComicsSaleCLI.rb
+++ lib/MyComicsSaleCLI.rb
@@ -30,7 +30,7 @@ puts""
 puts""    
 puts""   
 puts""  
-sleep(4.0)
+sleep(2.0)
 puts "What's your name?"    
 end    
     
@@ -39,24 +39,24 @@ def get_user_name
     self.username = name
     puts "Welcome, #{self.username}"
     puts "By using My Comics Sale App you will get the best comic book discounts"
-    sleep(4.0)
+    sleep(2.0)
     system("clear")
   end
     
 def start_scraping_wolverine    
 puts "We are about to list all the discount campaings "
-sleep(4.0)
+sleep(2.0)
 puts "Hold on we will ask for some help from Spider-man"
-sleep(4.0)
+sleep(2.0)
 puts "It looks like Spider-Man is stuck in Lincoln Tunnel"
-sleep(4.0)
+sleep(2.0)
 puts "But don’t worry, our app called X-Men" 
-sleep(4.0)
+sleep(2.0)
 puts "Wolverine is here to scrape all the great discounts with his adamantium claws" 
-sleep(4.0)
+sleep(2.0)
 MyComicsSaleScraper.scrape_comics_sale    
 puts "Thanks to Wolverine, here are all the comics on sale"
-sleep(4.0)
+sleep(2.0)
 end 
 
     
